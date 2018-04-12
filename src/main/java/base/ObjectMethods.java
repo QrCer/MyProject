1b0996033a4b483e44b8c9a9af57f45c26fabe8b
DongQirui <DongQirui@Baidu.com>
2018-04-12 11:28:47 +0800

    Format

--- src/main/java/base/ObjectMethods.java
+++ src/main/java/base/ObjectMethods.java
@@ -1,59 +1,66 @@
+/*  
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+
 package base;
 
 import java.lang.reflect.Method;
 
 /**
- * Created by QrCeric on 17/02/2017.
+ * Class ..........ObjectClone <br/>
+ * .
+ * @date ..........2018-04-12
+ * @author ........Dong.Qirui
  */
-class ObjectMethods {
+class ObjectClone extends Object implements Cloneable {
 
+    /**
+     * Field i ... <br/>
+     */
+    int i;
 
-    public static void main(String[] args) {
-        Object o = new Object();
+    /**
+     * Field name ... <br/>
+     */
+    String[] name;
 
-        ObjectFinalize objectFinalize = new ObjectFinalize();
-        System.out.println(objectFinalize.i);
+    @Override
+    protected Object clone() {
+        ObjectClone obj = null;
+
         try {
-            objectFinalize.finalize();
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
+            this.i   = 3;
+            obj      = (ObjectClone) super.clone();
+            obj.name = ((ObjectClone) super.clone()).name;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
         }
 
-//        try {
-//            objectFinalize.clone();
-//        } catch (CloneNotSupportedException e) {
-//            e.printStackTrace();
-//        }
-
-        ObjectClone objectClone = new ObjectClone();
-        objectClone.i= 2;
-        objectClone.name= new String[]{"AB"};
-        System.out.println(objectClone.i);
-        System.out.println("objectClone.name"+objectClone.name);
-        ObjectClone objectClone1 = (ObjectClone) objectClone.clone();
-        System.out.println("objectClone1.name"+objectClone1.name);
-        System.out.println(objectClone1.i);
-
-
-        final Method[] methods = o.getClass().getDeclaredMethods();
-
-        System.out.println("Object contains 9 methods: ");
-        for (Method method : methods) {
-            System.out.println(method.getName());
-        }
-
+        return obj;
     }
-
-
 }
 
+
+/**
+ * Class ..........ObjectFinalize <br/>
+ * .
+ * @date ..........2018-04-12
+ * @author ........Dong.Qirui
+ */
 class ObjectFinalize extends Object {
 
+    /**
+     * Field i ... <br/>
+     */
+    int i;
+
+    /**
+     * Constructor ObjectFinalize ... <br/>
+     * ------------------------------------
+     */
     ObjectFinalize() {
         System.out.println("Excuting construct();");
     }
-
-    int i;
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
@@ -65,27 +72,56 @@ class ObjectFinalize extends Object {
         System.out.println("Excuting finalize();");
         super.finalize();
     }
-
 }
 
-class ObjectClone extends Object implements Cloneable {
 
-    int i;
+/**
+ * Created by QrCeric on 17/02/2017.
+ */
+class ObjectMethods {
 
-    String[] name;
+    /**
+     * Method main ... <br/>
+     * .
+     * @param args
+     * .
+     * @author ........Dong.Qirui
+     */
+    public static void main(String[] args) {
+        Object         o              = new Object();
+        ObjectFinalize objectFinalize = new ObjectFinalize();
 
+        System.out.println(objectFinalize.i);
 
-
-    @Override
-    protected Object clone() {
-        ObjectClone obj = null;
         try {
-            this.i = 3;
-            obj = (ObjectClone) super.clone();
-            obj.name= ((ObjectClone) super.clone()).name;
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
+            objectFinalize.finalize();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
         }
-        return obj;
+
+        // try {
+        // objectFinalize.clone();
+        // } catch (CloneNotSupportedException e) {
+        // e.printStackTrace();
+        // }
+        ObjectClone objectClone = new ObjectClone();
+
+        objectClone.i    = 2;
+        objectClone.name = new String[] {"AB"};
+        System.out.println(objectClone.i);
+        System.out.println("objectClone.name" + objectClone.name);
+
+        ObjectClone objectClone1 = (ObjectClone) objectClone.clone();
+
+        System.out.println("objectClone1.name" + objectClone1.name);
+        System.out.println(objectClone1.i);
+
+        final Method[] methods = o.getClass().getDeclaredMethods();
+
+        System.out.println("Object contains 9 methods: ");
+
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        }
     }
 }
